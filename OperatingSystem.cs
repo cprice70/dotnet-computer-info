3aa58f47a74c7e5e6b2016ec1cd724e8ce870a05
Chris Price <cprice1@lci1.com>
2022-10-01 21:22:50 -0400

    Initial files

---
+++ OperatingSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ComputerInfo
+{
+    public static class OperatingSystem
+    {
+        public static bool IsWindows =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        public static bool IsMacOS =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static bool IsLinux =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        public static new string ToString => IsWindows ? "Windows" : (IsMacOS ? "MacOS" : "Linux");
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+var os = Environment.OSVersion;
+
+Console.WriteLine($"{ComputerInfo.OperatingSystem.ToString} {os.Version}");
+
